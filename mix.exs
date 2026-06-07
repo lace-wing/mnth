9d259024908a8e9bc86e0d51f88daa452acd259e
lace-wing <lycrlsu01@gmail.com>
2026-06-06 23:23:15 -0700

    cli: remove

--- mix.exs
+++ mix.exs
@@ -4,7 +4,6 @@ defmodule Mnth.MixProject do
   def project do
     [
       app: :mnth,
-      escript: escript(),
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
@@ -22,14 +21,7 @@ defmodule Mnth.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:color, "~> 0.13.0"},
-      {:optimus, "~> 0.6.1"}
-    ]
-  end
-
-  defp escript do
-    [
-      main_module: Mnth.CLI
+      {:color, "~> 0.13.0"}
     ]
   end
 end
